2dc2c31151bffee03a75433544df4cd81902cc9d
Ramazan Faruk Oğuz <farukoguz98@gmail.com>
2020-10-17 23:47:27 +0300

    Add files via upload

---
+++ YTKS/YazılımMuhendisligiTests/TicketTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YazılımMuhendisligi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YazılımMuhendisligi.Tests
+{
+    [TestClass()]
+    public class TicketTests
+    {
+        double p;
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            p = -15;
+        }
+
+        [TestMethod()]
+        public void TicketTest()
+        {         
+            Ticket t = new Ticket(p);
+            Assert.AreEqual(p, t.NetPrice);
+        }
+    }
+}
